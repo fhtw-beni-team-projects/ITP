b8169ae025aeca1d692dfd6234ad17b212820be0
Benedikt Galbavy <if22b082@technikum-wien.at>
2023-06-12 12:47:25 +0200

    join + fixes

--- chess-app/server/services/game-service.js
+++ chess-app/server/services/game-service.js
@@ -6,9 +6,9 @@ class GameService {
     games = new Map();
 
     createGame() {
-        gameId = this.generateGameId();
-        time = 900;
-        increment = 10;
+        let gameId = this.generateGameId();
+        let time = 900;
+        let increment = 10;
 
         this.games[gameId] = new Game(time, increment, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
         return gameId;
@@ -29,16 +29,38 @@ class GameService {
 
     join(gameId) {
         // Return either white or black, this will let the user know which color they are
+        if (this.games[gameId].black_joined == true && this.games[gameId].white_joined == true) {
+            return PlayerState.INVALID
+        }
 
-        /* Your logic here */
+        var color;
 
-        return PlayerState.WHITE // PlayerState.BLACK
+        var rand2 = Math.floor(Math.random() * 2);
+        if (this.games[gameId].black_joined == true || (rand2 == 0 && this.games[gameId].white_joined == false)) {
+            this.games[gameId].white_joined = true;
+            color = PlayerState.WHITE
+        } else {
+            this.games[gameId].black_joined = true;
+            color = PlayerState.BLACK
+        }
+
+        if (this.games[gameId].black_joined == true && this.games[gameId].white_joined == true) {
+            this.games[gameId].startTimer()
+        }
+
+        return color
     }
 
     move(gameId, who, move /* in SAN notation */) {
-        game = this.games[gameId];
+        let game = this.games[gameId];
+        if (this.games[gameId].state == "concluded") {
+            return this.games[gameId].stateObj
+        }
+
         if ((Date.now() - game.last_time) > game.time[game.game.turn()]) {
-            return game.game.turn() == 'w' ? GameState.BlackWon(this.#getPlayerStates(gameId), players, this.#getBoard(gameId)) : GameState.WhiteWon(this.#getPlayerStates(gameId), players, this.#getBoard(gameId))
+            this.games[gameId].state = "concluded"
+            this.games[gameId].stateObj = game.game.turn() == 'w' ? GameState.BlackWon(this.#getPlayerStates(gameId), players, this.#getBoard(gameId)) : GameState.WhiteWon(this.#getPlayerStates(gameId), players, this.#getBoard(gameId))
+            return this.games[gameId].stateObj
         }
 
         this.games[gameId].move(move);
@@ -46,11 +68,15 @@ class GameService {
         const players = this.#getPlayerStates(gameId)
         const board = this.#getBoard(gameId)
 
-        if (this.games[gameId].game.isCheckmate()) {
-            return this.games[gameId].game.turn() == 'w' ? GameState.BlackWon(gameId, players, board) : GameState.WhiteWon(gameId, players, board)
+        if (this.games[gameId].isCheckmate()) {
+            this.games[gameId].state = "concluded"
+            this.games[gameId].stateObj = this.games[gameId].turn() == 'w' ? GameState.BlackWon(gameId, players, board) : GameState.WhiteWon(gameId, players, board)
+            return this.games[gameId].stateObj
         }
-        if (this.games[gameId].game.isGameOver()) {
-            return GameState.Draw(gameId, players, board)
+        if (this.games[gameId].isDraw()) {
+            this.games[gameId].state = "concluded"
+            this.games[gameId].stateObj = GameState.Draw(gameId, players, board)
+            return this.games[gameId].stateObj
         }
 
         return GameState.Running(gameId, players, board)
@@ -72,8 +98,8 @@ class GameService {
 
     #getPlayerStates(gameId) {
         return {
-            white: new PlayerState(60),
-            black: new PlayerState(40)
+            white: new PlayerState(this.games[gameId].getSecondsWhite()),
+            black: new PlayerState(this.games[gameId].getSecondsBlack())
         }
     }
     #getBoard(gameId) {
